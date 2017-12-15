9204f2cf8c2eac0df377b9297aabad57e552c7aa
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2017-12-15 09:12:49 +0700

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+http://jadwalsholat.pkpu.or.id/monthly.php?type=2&id=67&m=12&y=2017
+<br>
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
