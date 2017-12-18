23dbba3dc444b9252cc175e28930b22648f66f6e
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2017-12-18 13:59:36 +0700

    add jadwal command handler

--- app/Commands/JadwalCommand.php
+++ app/Commands/JadwalCommand.php
@@ -4,6 +4,8 @@ namespace App\Commands;
 
 use Telegram\Bot\Actions;
 use Telegram\Bot\Commands\Command;
+use App\Schedule;
+use App\Registration;
 
 class JadwalCommand extends Command
 {
@@ -22,33 +24,30 @@ class JadwalCommand extends Command
      */
     public function handle($arguments)
     {
-        // This will send a message using `sendMessage` method behind the scenes to
-        // the user/chat id who triggered this command.
-        // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
-        // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
-        $this->replyWithMessage(['text' => 'Hello! Welcome to our bot, Here are our available commands:']);
+      $date = date("Y-m-d");
+      $text = "";
+      $chat_id = $this->getTelegram()->getWebhookUpdates()->getMessage()->getChat()->getId();
+      $registration = Registration::where('chat_id', $chat_id)->first();
+      if($registration){
+        $city_id = $registration->city_id;
+        $schedule = Schedule::where(
+          ['city_id' => $city_id],
+          ['tanggal' => $date]
+        )->first();
+        if($schedule){
+          $text .= "$date";
+          $text .= "\nShubuh : $schedule->shubuh";
+          $text .= "\nDzuhur : $schedule->dzuhur";
+          $text .= "\nAshr   : $schedule->ashr";
+          $text .= "\nMaghrib: $schedule->maghrib";
+          $text .= "\nIsya : $schedule->isya";
+        }else{
+          $text .= "Not available Schedule Yet";
+        }
+      }else{
+        $text .= "Not registered Yet";
+      }
+      $this->replyWithMessage(['text' => $text]);
 
-        // This will update the chat status to typing...
-        $this->replyWithChatAction(['action' => Actions::TYPING]);
-
-        // This will prepare a list of available commands and send the user.
-        // First, Get an array of all registered commands
-        // They'll be in 'command-name' => 'Command Handler Class' format.
-        // $commands = $this->getTelegram()->getCommands();
-        //
-        // // Build the list
-        // $response = '';
-        // foreach ($commands as $name => $command) {
-        //     $response .= sprintf('/%s - %s' . PHP_EOL, $name, $command->getDescription());
-        // }
-        //
-        // // Reply with the commands list
-        // $this->replyWithMessage(['text' => $response]);
-        //
-        // // Trigger another command dynamically from within this command
-        // // When you want to chain multiple commands within one or process the request further.
-        // // The method supports second parameter arguments which you can optionally pass, By default
-        // // it'll pass the same arguments that are received for this command originally.
-        // $this->triggerCommand('subscribe');
     }
 }
