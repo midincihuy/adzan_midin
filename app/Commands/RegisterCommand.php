1d5d9272e9809411b3a0f2c1c0968e97bac91dc8
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2017-12-18 13:45:11 +0700

    validation on register empty arguments

--- app/Commands/RegisterCommand.php
+++ app/Commands/RegisterCommand.php
@@ -25,37 +25,19 @@ class RegisterCommand extends Command
     public function handle($arguments)
     {
         $data = explode(' ', $arguments);
-
-        $chat_id = $this->getTelegram()->getWebhookUpdates()->getMessage()->getChat()->getId();
         $city_id = $data[0];
-        $registration = new Registration();
-        $registration->chat_id = $chat_id;
-        $registration->city_id = $city_id;
-        $registration->save();
+        if(!empty($city_id)){
+          $chat_id = $this->getTelegram()->getWebhookUpdates()->getMessage()->getChat()->getId();
 
-        $this->replyWithMessage(['text' => 'Hello! Thanks for registering']);
+          $registration = new Registration();
+          $registration->chat_id = $chat_id;
+          $registration->city_id = $city_id;
+          $registration->save();
 
-        // This will update the chat status to typing...
-        // $this->replyWithChatAction(['action' => Actions::TYPING]);
+          $this->replyWithMessage(['text' => 'Hello! Thanks for registering']);
+        }else{
+          $this->replyWithMessage(['text' => "Please provide the city_id\nExample : /register [city_id]"]);
 
-        // This will prepare a list of available commands and send the user.
-        // First, Get an array of all registered commands
-        // They'll be in 'command-name' => 'Command Handler Class' format.
-        // $commands = $this->getTelegram()->getCommands();
-        //
-        // // Build the list
-        // $response = '';
-        // foreach ($commands as $name => $command) {
-        //     $response .= sprintf('/%s - %s' . PHP_EOL, $name, $command->getDescription());
-        // }
-        //
-        // // Reply with the commands list
-        // $this->replyWithMessage(['text' => $response]);
-        //
-        // // Trigger another command dynamically from within this command
-        // // When you want to chain multiple commands within one or process the request further.
-        // // The method supports second parameter arguments which you can optionally pass, By default
-        // // it'll pass the same arguments that are received for this command originally.
-        // $this->triggerCommand('subscribe');
+        }
     }
 }
