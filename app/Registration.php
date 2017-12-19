3bbc5d0b2b106a2168b0ed5dd99e01e70fc3d2cf
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2017-12-19 13:38:00 +0700

    add fillable

--- app/Registration.php
+++ app/Registration.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Registration extends Model
 {
+  protected $fillable = [
+    'city_id', 'chat_id'
+  ];
   public function city()
   {
     return $this->belongsTo('App\City', 'city_id', 'city_id');
