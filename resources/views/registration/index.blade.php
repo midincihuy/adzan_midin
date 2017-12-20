b2824b451407c4ba07065b8d755cac6c363d5361
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2017-12-20 11:27:13 +0700

    add first name and last name in registration

--- resources/views/registration/index.blade.php
+++ resources/views/registration/index.blade.php
@@ -7,12 +7,12 @@
       <table id="registration">
         <thead>
           <tr>
-            <th>City</th><th>Type</th><th>Chat Title</th><th>Chat Username</th><th>Alert (minutes)</th>
+            <th>City</th><th>Type</th><th>Chat Title</th><th>Chat Username</th><th>First Name</th><th>Last Name</th><th>Alert (minutes)</th>
           </tr>
         </thead>
         <tfoot>
           <tr>
-            <th>City</th><th>Type</th><th>Chat Title</th><th>Chat Username</th><th>Alert (minutes)</th>
+            <th>City</th><th>Type</th><th>Chat Title</th><th>Chat Username</th><th>First Name</th><th>Last Name</th><th>Alert (minutes)</th>
           </tr>
         </tfoot>
       </table>
@@ -31,6 +31,8 @@
         { data: 'chat.type', name: 'chat.type' },
         { data: 'chat.title', name: 'chat.title' },
         { data: 'chat.username', name: 'chat.username' },
+        { data: 'chat.first_name', name: 'chat.first_name' },
+        { data: 'chat.last_name', name: 'chat.last_name' },
         { data: 'alert', name: 'alert' },
       ]
     });
