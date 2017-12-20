28510e7fd32a29f13e2bf5578866832e73628897
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2017-12-20 10:58:33 +0700

    complete the param

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -14,15 +14,15 @@ class WebhookController extends Controller
     foreach($updates as $update){
       $data = [
         'raw_updates' => json_encode($updates), // Testing
-        // 'id' => $update['update_id'],
-        // 'message_id' => $update['message']['message_id'],
-        // 'from_id' => $update['message']['from']['id'],
-        // 'chat_id' => $update['message']['chat']['id'],
-        // 'date' => $update['message']['date'],
-        // 'text' => isset($update['message']['text']) ? $update['message']['text'] : "",
-        // 'type' => isset($update['message']['entities']) ? $update['message']['entities'][0]['type'] : "",
-        // 'new_chat_member' => isset($update['message']['new_chat_member']) ? $update['message']['new_chat_member']['id'] : "",
-        // 'left_chat_member' => isset($update['message']['left_chat_member']) ? $update['message']['left_chat_member']['id'] : "",
+        'id' => $update['update_id'],
+        'message_id' => $update['message']['message_id'],
+        'from_id' => $update['message']['from']['id'],
+        'chat_id' => $update['message']['chat']['id'],
+        'date' => $update['message']['date'],
+        'text' => isset($update['message']['text']) ? $update['message']['text'] : "",
+        'type' => isset($update['message']['entities']) ? $update['message']['entities'][0]['type'] : "",
+        'new_chat_member' => isset($update['message']['new_chat_member']) ? $update['message']['new_chat_member']['id'] : "",
+        'left_chat_member' => isset($update['message']['left_chat_member']) ? $update['message']['left_chat_member']['id'] : "",
       ];
 
       $save = Update::firstOrCreate($data);
