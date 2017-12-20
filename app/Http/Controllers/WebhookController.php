830c15d772e004203c28fe05acf77f9c311daf6a
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2017-12-20 10:52:17 +0700

    disable sementara

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -11,22 +11,22 @@ class WebhookController extends Controller
   {
     /** @var \Telegram\Bot\Objects\Update $update */
     $update = \Telegram::commandsHandler(true);
-    foreach($updates as $update){
-      $data = [
-        'raw_updates' => json_encode($update), // Testing
-        // 'id' => $update['update_id'],
-        // 'message_id' => $update['message']['message_id'],
-        // 'from_id' => $update['message']['from']['id'],
-        // 'chat_id' => $update['message']['chat']['id'],
-        // 'date' => $update['message']['date'],
-        // 'text' => isset($update['message']['text']) ? $update['message']['text'] : "",
-        // 'type' => isset($update['message']['entities']) ? $update['message']['entities'][0]['type'] : "",
-        // 'new_chat_member' => isset($update['message']['new_chat_member']) ? $update['message']['new_chat_member']['id'] : "",
-        // 'left_chat_member' => isset($update['message']['left_chat_member']) ? $update['message']['left_chat_member']['id'] : "",
-      ];
-
-      $save = Update::firstOrCreate($data);
-    }
+    // foreach($updates as $update){
+    //   $data = [
+    //     'raw_updates' => json_encode($update), // Testing
+    //     // 'id' => $update['update_id'],
+    //     // 'message_id' => $update['message']['message_id'],
+    //     // 'from_id' => $update['message']['from']['id'],
+    //     // 'chat_id' => $update['message']['chat']['id'],
+    //     // 'date' => $update['message']['date'],
+    //     // 'text' => isset($update['message']['text']) ? $update['message']['text'] : "",
+    //     // 'type' => isset($update['message']['entities']) ? $update['message']['entities'][0]['type'] : "",
+    //     // 'new_chat_member' => isset($update['message']['new_chat_member']) ? $update['message']['new_chat_member']['id'] : "",
+    //     // 'left_chat_member' => isset($update['message']['left_chat_member']) ? $update['message']['left_chat_member']['id'] : "",
+    //   ];
+    //
+    //   $save = Update::firstOrCreate($data);
+    // }
     return 'ok';
   }
 }
