37c35173f50717de8d0ac247435c8c8c68158b99
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2017-12-20 11:13:49 +0700

    tampilan updates->update_id di db

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -13,7 +13,12 @@ class WebhookController extends Controller
     $updates = \Telegram::commandsHandler(true);
     // foreach($updates as $update){
       $data = [
-        'raw_updates' => "getType updates = ".gettype($updates)." dan json : ".json_encode($updates), // Testing
+        'raw_updates' => "getType updates = ".gettype($updates)." dan json : ".json_encode($updates). " data updates->update_id $updates->update_id", // Testing
+
+        /*
+        Data $updates
+{"update_id":847159001,"message":{"message_id":759,"from":{"id":151065522,"is_bot":false,"first_name":"Hamidin","last_name":"Hidayat","username":"midincihuy","language_code":"en-US"},"chat":{"id":151065522,"first_name":"Hamidin","last_name":"Hidayat","username":"midincihuy","type":"private"},"date":1513743093,"text":"\/jadwal","entities":[{"offset":0,"length":7,"type":"bot_command"}]}}
+        */
         // 'id' => $updates['update_id'],
         // 'message_id' => $update['message']['message_id'],
         // 'from_id' => $update['message']['from']['id'],
